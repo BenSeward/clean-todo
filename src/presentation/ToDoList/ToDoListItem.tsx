bdb02d357b9860406deb82a7a6dc39813c6d6a98
Ben Seward <ben.seward@googlemail.com>
2022-07-29 21:08:32 +0100

    Installed Sass and removed dark background

--- src/presentation/ToDoList/ToDoListItem.tsx
+++ src/presentation/ToDoList/ToDoListItem.tsx
@@ -3,12 +3,13 @@ import { useToDoList } from "../../application/services/todo/useToDoList";
 import { ToDoItem } from "../../application/types/todo.type";
 import { Checkbox } from "../shared/Checkbox";
 import { Input } from "../shared/Input";
+import styles from "./ToDoList.module.scss";
 
 export function ToDoListItem({ id, label }: ToDoItem) {
   const { getToDoItem, updateToDoItem } = useToDoList();
 
   return (
-    <div>
+    <div className={styles.todoItem}>
       <label htmlFor={`${id}`}>
         <Checkbox id={`${id}`} />
         <Input
