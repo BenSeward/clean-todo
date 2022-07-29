493a0ea90fcff611ac65aa84472bf31c59a6a5d4
Ben Seward <ben.seward@googlemail.com>
2022-07-29 15:51:45 +0100

    Removed some any types and added type to the todo state.

--- src/application/store/index.tsx
+++ src/application/store/index.tsx
@@ -1,5 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useToDoList } from "../services/todo/readToDoItems";
+import { StoreInterface } from "../types/store.type";
+import { ToDoItem } from "../types/todo.type";
 
 interface ProviderProps {
   children: JSX.Element | JSX.Element[];
@@ -8,25 +10,26 @@ interface ProviderProps {
 const Context = createContext<any>(null);
 
 export function ApplicationContextProvider({ children }: ProviderProps) {
-  const [toDoItems, setToDoItems] = useState();
+  const [toDoItems, setToDoItems] = useState<ToDoItem[]>();
+
   const { retreiveInitialToDoItems } = useToDoList();
-
-  useEffect(() => {
-    function initializeAsyncContext() {
-      if (!toDoItems) {
-        retreiveInitialToDoItems().then((res) => setToDoItems(res as any));
-      }
-    }
-
-    initializeAsyncContext();
-  }, [toDoItems, retreiveInitialToDoItems]);
 
   const values = {
     toDoItems,
     setToDoItems,
   };
 
-  return <Context.Provider value={values as any}>{children}</Context.Provider>;
+  useEffect(() => {
+    function initializeAsyncContext() {
+      if (!toDoItems) {
+        retreiveInitialToDoItems().then((res) => setToDoItems(res));
+      }
+    }
+
+    initializeAsyncContext();
+  }, [toDoItems, retreiveInitialToDoItems]);
+
+  return <Context.Provider value={values}>{children}</Context.Provider>;
 }
 
 export const useApplicationContext = () => useContext(Context);
